22c6511d31a783eb630c24bef3837d3813f20ca9
Nikhil Kumar <nikhilkumar.storage@gmail.com>
2025-03-08 14:29:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener("fetch", (event) => {
             const allowedUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
             
             if (userAgent !== allowedUserAgent) {
-                return new Response("<h2>Access Denied</h2><p>This webpage can only be viewed inside the official app.</p>", {
+                return new Response("<h2>Access Denied</h2><p>This webpage can only be viewed inside the Gtech-poly Unofficial app.</p>", {
                     headers: { "Content-Type": "text/html" }
                 });
             }
